0fcc39d2a0146edd310eb6fed94f5c3284531622
valsk <wangzlb@qq.com>
2016-05-23 20:55:36 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,16 +1,11 @@
 'use strict';
 
 function thousands_separators(num) {
-var nstr=num.toString(); 
- nStr += '';
-	x = nStr.split('.');
-	x1 = x[0];
-	x2 = x.length > 1 ? '.' + x[1] : '';
-	var rgx = /(\d+)(\d{3})/;
-	while (rgx.test(x1)) {
-		x1 = x1.replace(rgx, '$1' + ',' + '$2');
-	}
-	return x1 + x2;
+ if(!/^(\+|-)?(\d+)(\.\d+)?$/.test(num)){alert("wrong!");   return   num;}   
+    var   a   =   RegExp.$1,   b   =   RegExp.$2,   c   =   RegExp.$3;   
+    var   re   =   new   RegExp().compile("(\\d)(\\d{3})(,|$)");   
+    while(re.test(b))   b   =   b.replace(re,   "$1,$2$3");   
+    return   a   +""+   b   +""+   c;   
  }
 
 module.exports = thousands_separators;
