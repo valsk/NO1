86192b4b34c24c7f7815a290ff248d0a2deb5789
valsk <wangzlb@qq.com>
2016-05-23 23:00:28 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -2,24 +2,26 @@
 
 function thousands_separators(num) {
  var str_num = num + "";//转换成字符串
-var ret_num = "";
-var counter = 0;
-for(var i=str_num.length-1;i>=0;i--)
-{
+ var ret_num = "";
+ var counter = 0;
+ for(var i=str_num.length-1;i>=0;i--)
+ {
     ret_num = str_num.charAt(i) + ret_num;
     if(str_num.charAt(i)==".")
     {
          counter = 0;
          continue;
     }
-  counter++;
-  if(counter==3)
-  {
+  else{
+   counter++;
+   if(counter==3)
+   {
         counter = 0;
        if(i!=0&&str_num.charAt(i-1)!=".")
        ret_num = "," + ret_num;
   }
  }
+}
   return ret_num;
  }
 
