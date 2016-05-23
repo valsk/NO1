1dfb75ceaaa15c1561e65aa4944790e345d34797
valsk <wangzlb@qq.com>
2016-05-23 20:57:58 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,11 +1,20 @@
 'use strict';
 
 function thousands_separators(num) {
- if(!/^(\+|-)?(\d+)(\.\d+)?$/.test(num)){alert("wrong!");   return   num;}   
-    var   a   =   RegExp.$1,   b   =   RegExp.$2,   c   =   RegExp.$3;   
-    var   re   =   new   RegExp().compile("(\\d)(\\d{3})(,|$)");   
-    while(re.test(b))   b   =   b.replace(re,   "$1,$2$3");   
-    return   a   +""+   b   +""+   c;   
+ var str=num.toString();
+ var newStr=new Array(str.length+parseInt(str.length/3));   
+  var strArray=str.split("");
+  newStr[newStr.length-1]=strArray[strArray.length-1];   
+  var currentIndex=strArray.length-1;   
+  for(var i=newStr.length-1;i>=0;i--){   
+    if((newStr.length-i)%4==0){   
+      newStr[i]=",";   
+    }  
+    else{   
+      newStr[i]=strArray[currentIndex--];   
+    }   
+  }   
+  return newStr.join("") 
  }
 
 module.exports = thousands_separators;
