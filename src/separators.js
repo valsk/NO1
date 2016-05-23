b3ece9c4268a5b2bed3c712a05f3912a65af70c5
valsk <wangzlb@qq.com>
2016-05-23 21:24:21 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function thousands_separators(num) {
-number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g,'$&,');
+      var s=num.toString();
+      s=s.replace(/^(\d*)$/,"$1.");
+        s=(s+).replace(/(\d*\.\d\d)\d*/,"$1");
+        s=s.replace(".",",");
+        var re=/(\d)(\d{3},)/;
+        while(re.test(s))
+                s=s.replace(re,"$1,$2");
+        s=s.replace(/,(\d\d)$/,".$1");
+        return s.replace(/^\./,"0.")
 
  }
 
