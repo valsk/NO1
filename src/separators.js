2bfa777a202a0fa81bb971d51623779de7e15570
valsk <wangzlb@qq.com>
2016-05-23 22:32:33 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -9,18 +9,18 @@ for(var i=str_num.length-1;i>=0;i--)
 ret_num = str_num.charAt(i) + ret_num;
 if(str_num.charAt(i)==".")
 {
-counter = 0;
-continue;
+ counter = 0;
+ continue;
 }
-counter++;
-if(counter==3)
-{
-counter = 0;
-if(i!=0&&str_num.charAt(i-1)!=".")
-ret_num = "," + ret_num;
-}
-}
-return ret_num;
+ counter++;
+ if(counter==3)
+ {
+ counter = 0;
+ if(i!=0&&str_num.charAt(i-1)!=".")
+ ret_num = "," + ret_num;
+ }
+ }
+   return ret_num;
  }
 
 module.exports = thousands_separators;
