b31300927edb37d94ee37cd341bebd6d29cbbfef
valsk <wangzlb@qq.com>
2016-05-23 22:48:05 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -6,21 +6,21 @@ var ret_num = "";
 var counter = 0;
 for(var i=str_num.length-1;i>=0;i--)
 {
-ret_num = str_num.charAt(i) + ret_num;
-if(str_num.charAt(i)==".")
-{
- counter = 0;
- continue;
-}
- counter++;
- if(counter==3)
- {
- counter = 0;
- if(i!=0&&str_num.charAt(i-1)!=".")
- ret_num = "," + ret_num;
+    ret_num = str_num.charAt(i) + ret_num;
+    if(str_num.charAt(i)==".")
+    {
+         counter = 0;
+         continue;
+    }
+  counter++;
+  if(counter==3)
+  {
+        counter = 0;
+       if(i!=0&&str_num.charAt(i-1)!=".")
+       ret_num = "," + ret_num;
+  }
  }
- }
-   return ret_num;
+  return ret_num;
  }
 
 module.exports = thousands_separators;
