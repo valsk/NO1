7c0ab18d965c2d7af46069390a79d56a5dc993ec
valsk <wangzlb@qq.com>
2016-05-23 21:56:33 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,27 +1,13 @@
 'use strict';
 
 function thousands_separators(num) {
- var str_num = num + "";//转换成字符串
-var ret_num = "";
-var counter = 0;
-for(var i=str_num.length-1;i>=0;i--)
-{
-ret_num = str_num.charAt(i) + ret_num;
-if(str_num.charAt(i)==".")
-{
-counter = 0;
-continue;
-}
-counter++;
-if(counter==3)
-{
-counter = 0;
-if(i!=0&&str_num.charAt(i-1)!=".")
-ret_num = "," + ret_num;
-}
-
-}
-return ret_num;
+    var s = num; /**获取小数型数据**/  
+    s += "";  
+    if (s.indexOf(".") == -1)  /**如果没有小数点，在后面补个小数点和0**/  
+       if (/\.\d$/.test(s)) s += "0"; /**正则判断**/  
+       while (/\d{4}(\.|,)/.test(s)) /**符合条件则进行替换**/  
+          s = s.replace(/(\d)(\d{3}(\.|,))/, "$1,$2"); /**每隔3位添加一个**/  
+          return s;  
  }
 
 module.exports = thousands_separators;
